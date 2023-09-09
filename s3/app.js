47d253ea5979d05dd7b8ba80c1370c0af790a094
Rajesh13121991 <131757334+Rajesh13121991@users.noreply.github.com>
2023-09-09 16:07:22 +0530

    Update app.js

--- s3/app.js
+++ s3/app.js
@@ -1,44 +1,29 @@
-//provides stronger error checking and more secure code
-"use strict";
-
-// web server framework for Node.js applications
-const express = require("express");
-// allows requests from web pages hosted on other domains
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
-const fileName = "app.js";
-
-const { HttpException } = require("./HttpException.utils");
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
-const port = 8083;
+const port = 3003;
 
-// Parses incoming JSON requests
 app.use(express.json());
-app.use(cors());
 
-/** add reqId to api call */
-app.use(function (req, res, next) {
-  res.locals.reqId = uuidv4();
-  next();
-});
+app.get('/calculate', async (req, res) => {
+  const { a, b } = req.query;
 
-app.post("/add-sub", (req, res) => {
-  const {a=0, b=0} = req.body;
-  console.log(`A: ${a}, B: ${b}`);
+  try {
+    // Make a request to S1 to get the sum
+    const sumResponse = await axios.get(`http://s1:3001/sum?a=${a}&b=${b}`);
+    const sum = sumResponse.data.result;
 
-  //////////////////////////////////////
-  // Your logic to call S1 and S2 services to get the addition and subtraction
-  //////////////////////////////////////
+    // Make a request to S2 to get the difference
+    const diffResponse = await axios.get(`http://s2:3002/difference?a=${a}&b=${b}`);
+    const diff = diffResponse.data.result;
 
-});
-
-/** 404 error */
-app.all("*", (req, res, next) => {
-  const err = new HttpException(404, "Endpoint Not Found");
-  return res.status(err.status).send(err.message);
+    res.json({ sum, difference: diff });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
-  console.log("Start", fileName, `S3 App listening at http://localhost:${port}`);
+  console.log(`S3 listening at http://localhost:${port}`);
 });
